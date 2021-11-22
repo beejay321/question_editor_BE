196bfdd11fa42ce7b20be71eb905da7d33384544
beejay321 <jibodu.busola@gmail.com>
2021-11-22 10:10:04 +0100

    Update schema.js

--- src/models/questions/schema.js
+++ src/models/questions/schema.js
@@ -5,26 +5,32 @@ const questionSchema = new Schema(
   {
     title: { type: String },
     rows: [
-      [
-        {image:{ type: String }},
-        [
-          {
-            id: { type: String },
-            type:{ type: String, default :"image" } ,
-            name: { type: String },
-            value: { type: String },
-          },
-        ],
-        [
-          {
-            id: { type: String },
-            type: { type: String, default :"radio" } ,
-            name: { type: String },
-            value: { type: String },
-          },
-        ],
-      ],
+      [{ type: { type: String, default: "image" } }, { type: { type: String, default: "image" } }],
+      [{ image: { type: String }, text: { type: String }, type: { type: String, default: "radio" } }, { type: { type: String, default: "radio" } }],
+      [{ image: { type: String }, text: { type: String }, type: { type: String, default: "radio" } }, { type: { type: String, default: "radio" } }],
     ],
+
+    // rows: [
+    //   [
+    //     {image:{ type: String }},
+    //     [
+    //       {
+    //         id: { type: String },
+    //         type:{ type: String, default :"image" } ,
+    //         name: { type: String },
+    //         value: { type: String },
+    //       },
+    //     ],
+    //     [
+    //       {
+    //         id: { type: String },
+    //         type: { type: String, default :"radio" } ,
+    //         name: { type: String },
+    //         value: { type: String },
+    //       },
+    //     ],
+    //   ],
+    // ],
   },
   { timestamps: true }
 );
